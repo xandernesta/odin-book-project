f0da73536a262ba1d0dee0b20945af397a5b3870
Alex N <xander.nesta@gmail.com>
2022-10-06 15:52:25 -0400

    added more stlying and placeholder cards to build functionality for

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const bookContainer = document.querySelector('.books-container');
+
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -10,3 +14,29 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
+
+function displayBooks(){
+    for (let i=0; i < myLibrary.length; i+= 1){
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardDiv.setAttribute('data-index', i);
+        bookContainer.append(cardDiv);
+    }
+}
+
+const Twilight = new Book('Twilight', 'Stephenie Meyer', '498', false);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const myBook = new Book('My Book', 'Me', 69, true);
+const myBook2 = new Book('My 2nd Book', 'Me', 68, false);
+const myBook3 = new Book('My 2nd Book', 'Me', 68, false);
+addBookToLibrary(Twilight);
+addBookToLibrary(theHobbit);
+addBookToLibrary(myBook);
+addBookToLibrary(myBook2);
+addBookToLibrary(myBook3);
+addBookToLibrary(theHobbit);
+addBookToLibrary(myBook);
+addBookToLibrary(myBook2);
+addBookToLibrary(myBook3);
+
+window.onload = () => {displayBooks();}
